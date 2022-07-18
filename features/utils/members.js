a484724dcb1e3524f23e10d55890dd79de85db48
Andre Miranda <evilgoku@gmail.com>
2022-07-18 11:27:19 -0300

    remove tetz

--- features/utils/members.js
+++ features/utils/members.js
@@ -1,10 +1,7 @@
 const config = require('./../../config.json')
 
 const getRandomMembers = function (members, n) {
-    const sliced = members.sort(() => .5 - Math.random()).slice(0, n - 1)
-    sliced.push(config.tetz)
-
-    return sliced
+    return members.sort(() => .5 - Math.random()).slice(0, n)
 }
 
 module.exports = { getRandomMembers } 
