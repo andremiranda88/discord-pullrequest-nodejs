c2e3aa126e318a1dbf2b8e3e371c5578179b5cc0
Andre Miranda <evilgoku@gmail.com>
2022-07-19 19:59:21 -0300

    remove logs

--- features/utils/rotation.js
+++ features/utils/rotation.js
@@ -2,22 +2,14 @@ let ROTATE_STACK = {}
 
 const rotateStack = function (roleId, members, selectedMembers) {
     const stack = createRoleInStack(roleId, members, selectedMembers)
-    console.log(stack.create);
 
     if (stack.create) {
         return selectedMembers
     }
 
-
-    console.log('BEFORE');
-    console.log(selectedMembers);
-    console.log('\n\n');
     const newMembers = selectedMembers.map(m => {
         let usedMember = m
         if (stack.getRoleStack.includes(m)) {
-            console.log('HERE')
-            // usuario em members nao existente em RoleStack
-            // usuario diferente do atual
             usedMember = members.find(another => {
                 if (stack.getRoleStack.includes(another)) return false
                 if (another == usedMember) return false
@@ -28,11 +20,6 @@ const rotateStack = function (roleId, members, selectedMembers) {
         ROTATE_STACK[roleId].push(usedMember)
         return usedMember
     });
-    console.log('AFTER');
-    console.log(newMembers);
-
-    console.log('ROTATE STACK');
-    console.log(ROTATE_STACK[roleId]);
 
     return newMembers
 }
