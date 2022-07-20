f1d541e38996bd2b160dd81f6cc9c786df00de96
André Miranda <evilgoku@gmail.com>
2022-07-20 18:05:27 +0000

    change pull request command to db

--- features/pull-request/usecase.js
+++ features/pull-request/usecase.js
@@ -4,30 +4,30 @@ const { getArgs, validateParams } = require('../utils/params')
 const { getMemberListIdByRole } = require('../utils/roles')
 const { getRandomMembers } = require('../utils/members')
 const { deleteThread } = require("../utils/threads")
-const { rotateStack } = require("../utils/rotation")
+// const { rotateStack } = require("../utils/rotation")
 const { rotateStackWithDb } = require("../utils/dbRotation")
 
+// const pullRequest = async function (client, msg, members) {
+
+//     try {
+
+//         console.log(`\n\n-------------------------------------\n`)
+//         const params = validateParams(msg)
+//         const args = getArgs(msg, params)
+//         const author = msg.author.id
+//         const membersWithRole = getMemberListIdByRole(msg, args.role, members)
+//         const randomUsers = getRandomMembers(membersWithRole, author, 2)
+//         const rotate = rotateStack(args.role, membersWithRole, randomUsers)
+
+//         await createPullRequestThread(msg, args, client, rotate)
+//         console.log(`\n-------------------------------------\n\n`)
+
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+
 const pullRequest = async function (client, msg, members) {
-
-    try {
-
-        console.log(`\n\n-------------------------------------\n`)
-        const params = validateParams(msg)
-        const args = getArgs(msg, params)
-        const author = msg.author.id
-        const membersWithRole = getMemberListIdByRole(msg, args.role, members)
-        const randomUsers = getRandomMembers(membersWithRole, author, 2)
-        const rotate = rotateStack(args.role, membersWithRole, randomUsers)
-
-        await createPullRequestThread(msg, args, client, rotate)
-        console.log(`\n-------------------------------------\n\n`)
-
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-const pullRequestNew = async function (client, msg, members) {
 
     try {
 
@@ -63,4 +63,4 @@ const deletePullRequest = async function (client, msg) {
     }
 }
 
-module.exports = { pullRequest, deletePullRequest, pullRequestNew }
+module.exports = { pullRequest, deletePullRequest }
