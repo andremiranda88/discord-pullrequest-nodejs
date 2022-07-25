cf9ce386465cf15ca02141ccbfc11e496c750675
André Miranda <evilgoku@gmail.com>
2022-07-25 17:10:51 +0000

    adjust stack rule

--- features/utils/dbRotation.js
+++ features/utils/dbRotation.js
@@ -2,53 +2,54 @@ const { getDatabaseValue, setDatabaseValue } = require('../auth/database')
 
 const ROTATE_STACK = "randomUserRotationByRole"
 
-const rotateStackWithDb = async function (roleId, members, selectedMembers) {
-    const roleList = await createRoleInStack(roleId, members, selectedMembers)
-    if (roleList.create || roleList.getRoleStack[roleId] == undefined) {
-        return selectedMembers
+const rotateStackWithDb = async function(roleId, members, selectedMembers) {
+  const roleList = await createRoleInStack(roleId, members, selectedMembers)
+  if (roleList.create || roleList.getRoleStack[roleId] == undefined) {
+    return selectedMembers
+  }
+
+  const selectedRole = roleList.getRoleStack[roleId]
+  const newMembers = selectedMembers.map(m => {
+    let usedMember = m
+    if (selectedRole.includes(m)) {
+      usedMember = members.find(another => {
+        if (selectedRole.includes(another)) return false
+        if (another == usedMember) return false
+
+        return true
+      })
     }
+    roleList.getRoleStack[roleId].push(usedMember)
+    return usedMember
+  });
 
-    const selectedRole = roleList.getRoleStack[roleId]
-    const newMembers = selectedMembers.map(m => {
-        let usedMember = m
-        if (selectedRole.includes(m)) {
-            usedMember = members.find(another => {
-                if (selectedRole.includes(another)) return false
-                if (another == usedMember) return false
 
-                return true
-            })
-        }
-        roleList.getRoleStack[roleId].push(usedMember)
-        return usedMember
-    });
-
-  
-    await setDatabaseValue(ROTATE_STACK, roleList.getRoleStack)
-    return newMembers
+  await setDatabaseValue(ROTATE_STACK, roleList.getRoleStack)
+  return newMembers
 }
 
-const createRoleInStack = async function (roleId, members, selectedMembers) {
-    let stack = await getDatabaseValue(ROTATE_STACK)
-    let create = false
-    if (isInvalidValidStack(stack, members, roleId)) {
-        if(stack == null) stack = { }
-        stack[roleId] = selectedMembers
-        await setDatabaseValue(ROTATE_STACK, stack)
-        create = true
-    }
+const createRoleInStack = async function(roleId, members, selectedMembers) {
+  let stack = await getDatabaseValue(ROTATE_STACK)
+  let create = false
+  if (isInvalidValidStack(stack, members, roleId)) {
+    if (stack == null) stack = {}
+    stack[roleId] = selectedMembers
+    await setDatabaseValue(ROTATE_STACK, stack)
+    create = true
+  }
 
-    return { getRoleStack: stack, create }
+  return { getRoleStack: stack, create }
 }
 
-const isInvalidValidStack = function (stack, members, roleId) {
+const isInvalidValidStack = function(stack, members, roleId) {
   return (
-      stack == undefined || 
-      stack == null ||
-      stack.length === 0 ||
-      stack[roleId].length === 0 ||
-      stack[roleId].length == members.length || 
-      stack[roleId].length == (members.length - 1)
+    stack == undefined ||
+    stack == null ||
+    stack.length === 0 ||
+    stack[roleId] == undefined ||
+    stack[roleId].length === 0 ||
+    stack[roleId].length == members.length ||
+    stack[roleId].length == (members.length - 1)
   )
 }
 
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const server = express()
 
 server.all("/", (req, res) => {
-    res.send("Bot is running!")
+    res.send({ message: 'online' })
 })
 
 function keepAlive() {
