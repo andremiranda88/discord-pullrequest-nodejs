d68b24d7b2396d65e0e821d48f28469d994af88c
André Miranda <evilgoku@gmail.com>
2022-08-03 17:50:07 +0000

    rotateStack with db

--- features/pull-request/usecase.js
+++ features/pull-request/usecase.js
@@ -27,41 +27,41 @@ const { rotateStackWithDb } = require("../utils/dbRotation")
 //     }
 // }
 
-const pullRequest = async function (client, msg, members) {
+const pullRequest = async function(client, msg, members) {
 
-    try {
+  try {
 
-        console.log(`\n\n-------------------------------------\n`)
-        const params = validateParams(msg)
-        const args = getArgs(msg, params)
-        const author = msg.author.id
-        const membersWithRole = getMemberListIdByRole(msg, args.role, members)
-        const membersWithoutAuthor = removeAuthor(membersWithRole, author)
-        const randomUsers = getRandomMembers(membersWithoutAuthor, 2)
-        const rotate = rotateStack(args.role, membersWithoutAuthor, randomUsers)
+    console.log(`\n\n-------------------------------------\n`)
+    const params = validateParams(msg)
+    const args = getArgs(msg, params)
+    const author = msg.author.id
+    const membersWithRole = getMemberListIdByRole(msg, args.role, members)
+    const membersWithoutAuthor = removeAuthor(membersWithRole, author)
+    const randomUsers = getRandomMembers(membersWithoutAuthor, 2)
+    const rotate = rotateStackWithDb(args.role, membersWithoutAuthor, randomUsers)
 
-        await createPullRequestThread(msg, args, client, rotate)
-        console.log(`\n-------------------------------------\n\n`)
+    await createPullRequestThread(msg, args, client, rotate)
+    console.log(`\n-------------------------------------\n\n`)
 
-    } catch (e) {
-        console.log(e)
-    }
+  } catch (e) {
+    console.log(e)
+  }
 }
 
-const deletePullRequest = async function (client, msg) {
+const deletePullRequest = async function(client, msg) {
 
-    try {
+  try {
 
-        console.log(`\n\n-------------------------------------\n`)
-        const param = msg.content.replace(".pull-request --delete ", "")
-        console.log(`Delete thread with Name[${param}]`);
-        await deleteThread(param, msg, client);
-        console.log(`Delete thread successfully with Name[${param}]`);
-        console.log(`\n-------------------------------------\n\n`)
+    console.log(`\n\n-------------------------------------\n`)
+    const param = msg.content.replace(".pull-request --delete ", "")
+    console.log(`Delete thread with Name[${param}]`);
+    await deleteThread(param, msg, client);
+    console.log(`Delete thread successfully with Name[${param}]`);
+    console.log(`\n-------------------------------------\n\n`)
 
-    } catch (e) {
-        console.log(e)
-    }
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 module.exports = { pullRequest, deletePullRequest }
